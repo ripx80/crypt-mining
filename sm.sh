ed0a5b867c51657482bddfb5a19bcb669a9b20eb
Ripx80 <ripx80@protonmail.com>
2018-01-05 09:49:52 +0100

    add readme

--- sm.sh
+++ sm.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+#
+# Dev by Ripx80
+# Supported cryptonight algo
+# https://easyhash.io/
+# POOLS:
+#   Monero
+#   stratum+tcp://pool.supportxmr.com
+#   stratum+tcp://pool.minexmr.com
+#
+#   IntenseCoin
+#   stratum+tcp://itns.easyhash.io:3730#   
+#
+#   Electroneum
+#   stratum+tcp://etn.easyhash.ko:3630
+#
+#   Sumokoin
+#   stratum+tcp://sumo.easyhash.io:3330
 POOL="stratum+tcp://pool.supportxmr.com"
 #POOL="stratum+tcp://pool.minexmr.com"
 # PORTS: 3333, 4444, 5555, 7777
 PORT=5555
 ADDR="42ATuGxCfG2eNvEG1WsSq83kptkcGQdDYerSDTg5pA8W5Fu4vyjzpfXFSojgZpoCQkeiFn87jDWS1WbgnPS25fVmR6sZ6CX"
-WORKER="$(hostname)"
+WORKER="$(hostname):8569826187d"
 THREADS="$(nproc)"
 HUGEPAGES=1
 TYPE="linux/cpu"
---
+++ tar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/tar -cf build/sm.tar address.txt autostart install.sh miners sm.sh uninstall.sh
+exit 0
