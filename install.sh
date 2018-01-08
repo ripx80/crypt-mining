3976f75397f2f59c1d925d163f67547d0ecafda8
Ripx80 <ripx80@protonmail.com>
2018-01-08 10:45:38 +0100

    refresh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-mkdir -p /usr/share/sm/
-cp -r miners sm.sh /usr/share/sm/
+#check inet connection with nc
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    git clone https://github.com/ripx80/crypt-mining/ /usr/share/sm
+else
+    mkdir -p /usr/share/sm
+    cp -r miners sm.sh .git /usr/share/sm/
+fi
+
 chmod +x /usr/share/sm/sm.sh
 cd autostart/systemd
 /bin/bash install.sh
