bbfe710fb00e037bb9d951e850c4c8e8382aa99b
Ripx80 <ripx80@protonmail.com>
2018-01-08 11:21:00 +0100

    add git pull cron timer

--- autostart/systemd/install.sh
+++ autostart/systemd/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cp rc.local /etc/
 cp rc-local.service /etc/systemd/system/
+cp rc-local-restart.timer /etc/systemd/system/
+cp rc-local-restart.service /etc/systemd/system/
 chmod +x /etc/rc.local
+systemctl enable --now rc-local-restart.timer
 systemctl enable rc-local
 systemctl start rc-local.service
 #systemctl status rc-local.service
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,9 @@ rm -r /usr/share/sm
 rm /etc/rc.local
 systemctl stop rc-local
 systemctl disable rc-local
+systemctl disable rc-local-restart.timer
 rm /etc/systemd/system/rc-local.service
+rm /etc/systemd/system/rc-local-restart.service
+rm /etc/systemd/system/rc-local-restart.timer
 echo "uninstalled"
 exit 0
